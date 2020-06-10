19546461aae5ecf6215a314152d975f16a3f3922
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-06-09 20:43:25 -0400

    wrote out all the questions, answers, and correct answers

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -7,47 +7,63 @@ let triviaQuestions = [
     //     incorrectAnswer: "<img src='assets/images/soSad.gif' />",
     // },
     
-    {question: " ",
+    {question: "Which of the following characters was never a Defense against the Dark Arts teacher?",
+            answers: ["Remus Lupin", "Sturgis Podmore", "Gilderoy Lockhart" , "Severus Snape"],
+            theAnswer: 1,
 
     },
 
-    {question: " ",
+    {question: "Which of the following classes was taught by Professor Flitwick?",
+            answers: ["Arithmancy", "Transformation", "Charms" , "Astronomy"],
+            theAnswer: 2,
 
     },
 
-    {question: " ",
+    {question: "Who are the two Divination teachers?",
+            answers: ["Firenze and Lupin", "Lupin and Trewlany", "Firenze and Trewlany" , "Sprout and Trewlany"],
+            theAnswer: 2,
+    },
+
+    {question: "Which of the following classes was taught by Professor Sprout?",
+            answers: ["Herbology", "Care of Magical Creatures", "Muggle Studies" , "Potions"],
+            theAnswer: 0,
 
     },
 
-    {question: " ",
+    {question: "Which of the following characters is NOT an animagus?",
+            answers: ["Sirius Black", "Peter Pettigrew", "James Potter" , "Remus Lupin"],
+            theAnswer: 3,
 
     },
 
-    {question: " ",
-
+    {question: "Which of the following people took the locket-horcrux from Voldemort's cave and replaced it with a decoy?",
+            answers: ["Sirius Black", "Regulus Black", "Bathilda Bagshot" , "Rita Skeeter"],
+            theAnswer: 1,
     },
 
-    {question: " ",
-
+    {question: "Who is the heir of Slytherin?",
+            answers: ["Lucius Malfoy", "Blaise Zabini", "Severus Snape" , "Tom Riddle"],
+            theAnswer: 1,
     },
 
-    {question: " ",
-
+    {question: "Which of the following is NOT in Diagon Alley?",
+            answers: ["Ollivander's Wand shop", "Honeydukes Sweets", "Madame Malkin's robe shop", "Flourish & Botts book shop"],
+            theAnswer: 1,
     },
 
-    {question: " ",
-
+    {question: "Which of the following is NOT in Hogsmeade?",
+            answers: ["Madame Puddifoot's Tea shop", "The Shrieking Shack", "Gringott's Bank", "The Three Broomsticks"],
+            theAnswer: 2,
     },
 
-    {question: " ",
-
+    {question: "Which of the following characters (surprisingly) comes to Harry's aid, ultimatley contributing to Voldemort's defeat?",
+            answers: ["Narcissa Malfoy", "Fenrir Greyback", "Alecto Carrow", "Amycus Carrow"],
+            theAnswer: 0,
     },
 
-    {question: " ",
-
-    },
-
-    {question: " ",
+    {question: "Which of the following characters caused the destruction of a horcrux without actually meaning to destroy it?",
+            answers: ["Mundungus Fletcher", "Xenophilius Lovegood", "Bathilda Bagshot", "Vincent Crabbe"],
+            theAnswer: 3,
 
     }
 
