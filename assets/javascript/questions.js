688e4cb993ac7f36c00adfa8ddf2e9a93390cad3
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-06-09 21:16:12 -0400

    added gifs for correct answers

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -11,67 +11,76 @@ let triviaQuestions = [
     {question: "Which of the following characters was never a Defense against the Dark Arts teacher?",
             answers: ["Remus Lupin", "Sturgis Podmore", "Gilderoy Lockhart" , "Severus Snape"],
             theAnswer: 1,
-
+            correctAnswer: "<img src='assets/images/lockhart1.gif' />",
+            incorrectAnswer:  "<img src='assets/images/nope3.gif' />",
     },
     //2
     {question: "Which of the following classes was taught by Professor Flitwick?",
             answers: ["Arithmancy", "Transformation", "Charms" , "Astronomy"],
             theAnswer: 2,
-
+            correctAnswer: "<img src='assets/images/flitwick2.gif' />",
     },
     //3
     {question: "Who are the two Divination teachers?",
             answers: ["Firenze and Lupin", "Lupin and Trelawny", "Firenze and Trelawny" , "Sprout and Trelawny"],
             theAnswer: 2,
+            correctAnswer: "<img src='assets/images/Trelawny3.gif' />",
     },
     //4
     {question: "Which of the following classes was taught by Professor Sprout?",
             answers: ["Herbology", "Care of Magical Creatures", "Muggle Studies" , "Potions"],
             theAnswer: 0,
+            correctAnswer: "<img src='assets/images/mandrake4.gif' />",
 
     },
     //5
     {question: "Which of the following characters is NOT an animagus?",
             answers: ["Sirius Black", "Peter Pettigrew", "James Potter" , "Remus Lupin"],
             theAnswer: 3,
+            correctAnswer: "<img src='assets/images/lupin5.gif' />",
 
     },
     //6
     {question: "Which of the following people took the locket-horcrux from Voldemort's cave and replaced it with a decoy?",
             answers: ["Sirius Black", "Regulus Black", "Bathilda Bagshot" , "Rita Skeeter"],
             theAnswer: 1,
+            correctAnswer: "<img src='assets/images/voldemort6.gif' />",
     },
     //7
     {question: "Who is the heir of Slytherin?",
             answers: ["Lucius Malfoy", "Blaise Zabini", "Severus Snape" , "Tom Riddle"],
             theAnswer: 1,
+            correctAnswer: "<img src='assets/images/riddle7.gif' />",
     },
     //8
     {question: "Which of the following is NOT in Diagon Alley?",
             answers: ["Ollivander's Wand shop", "Honeydukes Sweets", "Madame Malkin's robe shop", "Flourish & Botts book shop"],
             theAnswer: 1,
+            correctAnswer: "<img src='assets/images/fredandgeorge8.gif' />",
     },
     //9
     {question: "Which of the following is NOT in Hogsmeade?",
             answers: ["Madame Puddifoot's Tea shop", "The Shrieking Shack", "Gringott's Bank", "The Three Broomsticks"],
             theAnswer: 2,
+            correctAnswer: "<img src='assets/images/hogsmeade9.gif' />",
     },
     //10
     {question: "Which of the following characters (surprisingly) comes to Harry's aid, ultimatley contributing to Voldemort's defeat?",
             answers: ["Narcissa Malfoy", "Fenrir Greyback", "Alecto Carrow", "Amycus Carrow"],
             theAnswer: 0,
+            correctAnswer: "<img src='assets/images/draco10.gif' />",
     },
     //11
     {question: "Which of the following characters caused the destruction of a horcrux without actually meaning to destroy it?",
             answers: ["Mundungus Fletcher", "Xenophilius Lovegood", "Bathilda Bagshot", "Vincent Crabbe"],
             theAnswer: 3,
-
+            correctAnswer: "<img src='assets/images/reading11.gif' />",
     },
 
     {question: "How did the Snatchers/Death Eaters know that Harry, Ron and Hermione were in Hogsmeade just prior to the Battle at Hogwarts?",
-    answers: ["Someone had been tracking them through an enchantment", "Griphook betrayed them after the Gringott's stint", "Harry saying the name 'Voldemort' triggered the taboo curse", "They were seen flying on broomsticks"],
-    theAnswer: 2,
-
-}
+            answers: ["Someone had been tracking them through an enchantment", "Griphook betrayed them after the Gringott's stint", "Harry saying the name 'Voldemort' triggered the taboo curse", "They were seen flying on broomsticks"],
+            theAnswer: 2,
+            correctAnswer: "<img src='assets/images/deatheaters12.gif' />",
+    }
 
 ] 
