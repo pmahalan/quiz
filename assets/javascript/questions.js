bb9bd4a4604ce75d2ea4a5640913f1055cf67bce
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-06-09 20:00:32 -0400

    added code to questions.js to set up the structure of the array of questions

---
+++ assets/javascript/questions.js
@@ -0,0 +1,50 @@
+let triviaQuestions = [
+
+    // {question: "Where did Meredith Grey go to College?", 
+    //     answers: ["Darmouth College", "Princeton University" , "Prescott College" , "Stanford University"],
+    //     theAnswer: 0,
+    //     correctAnswer: "<img src='assets/images/darmouth.gif' />",
+    //     incorrectAnswer: "<img src='assets/images/soSad.gif' />",
+    // },
+    
+    {question: " ",
+
+    },
+
+    {question: " ",
+
+    },
+
+    {question: " ",
+
+    },
+
+    {question: " ",
+
+    },
+
+    {question: " ",
+
+    },
+
+    {question: " ",
+
+    },
+
+    {question: " ",
+
+    },
+
+    {question: " ",
+
+    },
+
+    {question: " ",
+
+    },
+
+    {question: " ",
+
+    }
+
+] 
